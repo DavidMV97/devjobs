08a6773e984a32a9adf0e7360fa34f33e4f12695
Germán David <manchov12@gmail.com>
2025-08-25 20:24:30 -0500

    Creando seeder salario

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,3 +1,55 @@
-<div>
-    desde livewire
-</div>
+<form action="" class="md:w-1/2 space-y-5">
+    <div>
+        <x-input-label for="titulo" :value="__('Título vacante')" />
+        <x-text-input id="titulo" class="block mt-1 w-full" type="text" name="titulo" :value="old('titulo')"
+            placeholder="Título vacante" />
+    </div>
+
+    <div>
+        <x-input-label for="salario" :value="__('Salario Mensual')" />
+        <select name="salario" id="salario"
+            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full">
+        </select>
+    </div>
+
+    <div>
+        <x-input-label for="categoria" :value="__('Categoría')" />
+        <select name="salario" id="salario"
+            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full">
+            <option>-- Seleccione --</option>
+            @foreach ($salarios as $salario)
+                <option value="{{ $salario->id }}"> {{ $salario->salario }} </option>
+            @endforeach
+        </select>
+    </div>
+
+    <div>
+        <x-input-label for="empresa" :value="__('Empresa')" />
+        <x-text-input id="empresa" class="block mt-1 w-full" type="text" name="empresa" :value="old('empresa')"
+            placeholder="Empresa: ej. Netflix, Uber, Shopify" />
+    </div>
+
+
+    <div>
+        <x-input-label for="ultimo_dia" :value="__('Último día para postularse')" />
+        <x-text-input id="ultimo_dia" class="block mt-1 w-full" type="date" name="ultimo_dia" :value="old('ultimo_dia')" />
+    </div>
+
+    <div>
+        <x-input-label for="descripcion" :value="__('Descripción puesto')" />
+        <textarea name="descripcion" id="descripcion" placeholder="Descripción general del puesto, experiencia"
+            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full h-72">
+        </textarea>
+    </div>
+
+    <div>
+        <x-input-label for="imagen" :value="__('Imagen')" />
+        <x-text-input id="imagen" class="block mt-1 w-full" type="file" name="titulo"
+        />
+    </div>
+
+    <x-primary-button>
+        Crear Vacante
+    </x-primary-button>
+
+</form>
