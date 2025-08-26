d02ffa1eb6e1b4dc0f2d881a86ebfc5351977651
Germán David <manchov12@gmail.com>
2025-08-26 14:18:44 -0500

    Guardando vacante

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Categoria;
 use App\Models\Salario;
+use App\Models\Vacante;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -16,7 +17,7 @@ class CrearVacante extends Component
     public $empresa;
     public $ultimo_dia;
     public $descripcion;
-    public $imagen; 
+    public $imagen;
 
     use WithFileUploads;
 
@@ -31,17 +32,36 @@ class CrearVacante extends Component
         'imagen' => 'required|image|max:1024'
     ];
 
-    public function crearVacante(){
+    public function crearVacante()
+    {
         $datos = $this->validate();
-        
+
         // Almacenar imagen
+        $imagen = $this->imagen->store('public/vacantes');
+        $datos['imagen'] = str_replace('public/vacantes/', '', $imagen);
+
+        // Crear vacante
+        Vacante::create([
+            'titulo' => $datos['titulo'],
+            'salario_id'  => $datos['salario'],
+            'categoria_id'  => $datos['categoria'],
+            'empresa'  => $datos['empresa'],
+            'ultimo_dia'  => $datos['ultimo_dia'],
+            'descripcion'  => $datos['descripcion'],
+            'imagen'  => $datos['imagen'],
+            'user_id'  => auth()->user()->id
+        ]);
+
+        session()->flash('mensaje', 'La Vacante se publicó correctamente');
+
+        return redirect()->route('vacantes.index'); 
     }
 
     public function render()
     {
         // consultar db
-        $salarios = Salario::all(); 
-        $categorias = Categoria::all(); 
+        $salarios = Salario::all();
+        $categorias = Categoria::all();
         return view('livewire.crear-vacante', [
             'salarios' => $salarios,
             'categorias' => $categorias
